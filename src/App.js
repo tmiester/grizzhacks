ae6a5dcd2ded9e350a883fec0559738455098acd
tmiester <159564860+tmiester@users.noreply.github.com>
2025-03-22 21:22:44 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,26 @@
+import React, { useEffect, useState } from "react";
+
+function App() {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        fetch("/data.json")  // Fetches the JSON file from the "public" folder
+            .then(response => response.json())
+            .then(json => setData(json))
+            .catch(error => console.error("Error loading JSON:", error));
+    }, []);
+
+    return (
+        <div>
+            <h1>GrizzHacks Data</h1>
+            <ul>
+                {data.map((item, index) => (
+                    <li key={index}>{item.name} - {item.value}</li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default App;
 
