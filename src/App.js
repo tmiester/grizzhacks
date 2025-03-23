eaf4c9886d727f04a126198f551dca8649ddf1b1
tmiester <159564860+tmiester@users.noreply.github.com>
2025-03-23 00:12:04 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,28 +3,9 @@ import React from "react";
 function App() {
   return (
     <div>
-      <h1>Click the Fishbowl!</h1>
-      <img 
-        src="/fishbowl.jpg" 
-        alt="Fishbowl" 
-        style={{ cursor: "pointer", width: "300px" }} 
-        onClick={handleClick} 
-      />
+      <h1>Hello, GrizzHacks! 🚀</h1>
     </div>
   );
 }
 
-function handleClick() {
-  fetch("/data.json")  // Fetching from public/data.json
-    .then(response => response.json())
-    .then(data => alert(JSON.stringify(data, null, 2)))
-    .catch(error => console.error("Error fetching data:", error));
-}
-
 export default App;
-
-export default App;
-
-
-export default App;
-
