821acddcd1b527832619d1bd7bff88a118867854
tmiester <159564860+tmiester@users.noreply.github.com>
2025-03-22 23:50:35 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,36 +1,27 @@
-import React, { useState } from "react";
+import React from "react";
 
-const App = () => {
-  const [data, setData] = useState(null);
-
-  const handleClick = async () => {
-    try {
-      const response = await fetch("/data.json");
-      const jsonData = await response.json();
-      setData(jsonData);
-    } catch (error) {
-      console.error("Error fetching data:", error);
-    }
-  };
-
+function App() {
   return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>Click the Fishbowl</h1>
-      <img
-        src="/fishbowl.jpg"
-        alt="Fishbowl"
-        onClick={handleClick}
-        style={{ width: "300px", cursor: "pointer" }}
+    <div>
+      <h1>Click the Fishbowl!</h1>
+      <img 
+        src="/fishbowl.jpg" 
+        alt="Fishbowl" 
+        style={{ cursor: "pointer", width: "300px" }} 
+        onClick={handleClick} 
       />
-      {data && (
-        <div style={{ marginTop: "20px", border: "1px solid black", padding: "10px" }}>
-          <h2>Database Info:</h2>
-          <pre>{JSON.stringify(data, null, 2)}</pre>
-        </div>
-      )}
     </div>
   );
-};
+}
+
+function handleClick() {
+  fetch("/data.json")  // Fetching from public/data.json
+    .then(response => response.json())
+    .then(data => alert(JSON.stringify(data, null, 2)))
+    .catch(error => console.error("Error fetching data:", error));
+}
+
+export default App;
 
 export default App;
 
